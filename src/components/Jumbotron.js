c048ba8ec111f77b96c683d1792fbb821f63594f
Kartik Patidar <kartikpatidar815@gmail.com>
2020-07-02 00:01:32 +0530

    Jumbotron Button Added and moved css to external file

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,61 +1,19 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
-import styled from 'styled-components';
-import CansBackground from '../assets/images/cans-background.jpg';
+import classes from './Jumbotron.module.css';
 
-const Styles = styled.div`
-    .jumbo {
-        display: flex;
-        align-items: center;
-        background: url(${CansBackground}) no-repeat fixed bottom;
-        background-size: cover;
-        color: #ccc;
-        height: 100vh;
-        position: relative;
-        z-index: -2;
-    }
-    .overlay {
-        background-color: #000;
-        opacity: 0.6;
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        z-index: -1;
-        
-    }
-    .text-header {
-        text-align: center;
-        z-index: 0;
-        opacity: 1;
-        font-size: 5rem;
-        color: white;
-        font-family: "Poppins", sans-serif;
-    }
-    .text-header-para {
-        z-index: 0;
-        opacity: 1;
-        text-align: center;
-        font-size: 2rem;
-        color: #efefef;
-        font-family: "Poppins", sans-serif;
-    }
-    .container{
-        margin-bottom:2rem;
-    }
-`;
 
 const Jumbotron = () => (
-    <Styles>
-        <Jumbo fluid className="jumbo">
-            <div className="overlay"></div>
-            <Container>
-                <h1 className="text-header">Welcome to <b> Kachra Seth </b></h1>
-                <p className="text-header-para">Sell Your Trash at your <b>door step </b> and contribute in <b>proper waste managment</b>.So that we can make our country <b>green and clean. </b></p>
-            </Container>
-        </Jumbo>
-    </Styles>
+    <Jumbo fluid className={classes.jumbo}>
+        <div className={classes.overlay}></div>
+        <Container>
+            <h1 className={classes.textHeader}>Welcome to <b> Kachra Seth </b></h1>
+            <p className={classes.textHeaderPara}>Sell Your Trash at your <b>door step </b> and contribute in <b>proper waste managment</b>.So that we can make our country <b>green and clean. </b></p>
+            <div className={classes.myCoutainer}>
+            <a href="/login" className={classes.jumboButton}><b>Get Started</b></a>  
+            </div>
+        </Container>
+    </Jumbo>
 );
 
 
