f67c4207be46c5e0e74c5d6215f5873b875577a6
Milind Phalke <milindphalke30@gmail.com>
2021-10-08 20:05:56 +0530

    Added the Schemas.First Commit

---
+++ BackEnd/models/Order.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+//const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  news: { type: Number, required: true },
+  card: { type: Number, required: true },
+  books: { type: Number, required: true },
+  plastic: { type: Number, required: true },
+  aluminium: { type: Number, required: true },
+  ac: { type: Number, required: true },
+  fridge: { type: Number, required: true },
+  washing: { type: Number, required: true },
+  battery: { type: Number, required: true },
+  ewaste: { type: Number, required: true },
+  cpu: { type: Number, required: true },
+  lcd: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  acquired: { type: Boolean, required: true },
+  owner: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+});
+
+//userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Order", userSchema);
