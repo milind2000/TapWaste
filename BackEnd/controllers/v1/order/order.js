27f5ca9394ea022c886c9c5c65ab6e48563b5e47
kartik Patidar <kartikpatidar815@gmail.com>
2021-10-17 18:05:12 +0530

    changes after merger 2

--- BackEnd/controllers/v1/order/order.js
+++ BackEnd/controllers/v1/order/order.js
@@ -1,46 +1,21 @@
 const Item = require("../../../models/Order.js");
-const addItem = async function (req, res){ 
-    const news = req.body.news;
-    const card = req.body.card; 
-    const books = req.body.books;
-    const plastic = req.body.plastic;
-    const aluminium = req.body.aluminium;
-    const ac = req.body.ac;
-    const fridge = req.body.fridge;
-    const washing = req.body.washing;
-    const battery = req.body.battery;
-    const ewaste = req.body.ewaste;
-    const cpu = req.body.cpu;
-    const lcd = req.body.lcd;
-    const amount = req.body.amount;
-    const acquired = req.body.acquired;
-    const owner = req.body.owner;  
-    item = new Item({
-            news: news,
-            card: card,
-            books: books, 
-            plastic: plastic,
-            aluminium: aluminium,
-            ac: ac,
-            fridge: fridge,
-            washing: washing,
-            battery: battery,
-            ewaste: ewaste,
-            cpu: cpu,
-            lcd: lcd,
-            amount: amount,
-            acquired: acquired,
-            owner: owner
-
-        });
-        await product.save();
-        res.status(200).send({
-        message: "Created Successfully",
-        itemData: {
-            item
-        },
-        });
-    }
-    module.exports={
-        addItem: addItem
-    };
+const addItem = async function (req, res) {
+  const amount = req.body.amount;
+  const acquired = req.body.acquired;
+  const owner = req.body.owner;
+  const item = new Item({
+    amount: amount,
+    acquired: acquired,
+    owner: owner,
+  });
+  await item.save();
+  res.status(200).send({
+    message: "Created Successfully",
+    itemData: {
+      item,
+    },
+  });
+};
+module.exports = {
+  addItem: addItem,
+};
--- BackEnd/models/Order.js
+++ BackEnd/models/Order.js
@@ -4,20 +4,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-  news: { type: Number, required: true },
-  card: { type: Number, required: true },
-  books: { type: Number, required: true },
-  plastic: { type: Number, required: true },
-  aluminium: { type: Number, required: true },
-  ac: { type: Number, required: true },
-  fridge: { type: Number, required: true },
-  washing: { type: Number, required: true },
-  battery: { type: Number, required: true },
-  ewaste: { type: Number, required: true },
-  cpu: { type: Number, required: true },
-  lcd: { type: Number, required: true },
   amount: { type: Number, required: true },
-  acquired: { type: Boolean, required: true },
+  acquired: { type: Boolean, required: true, default: false },
   owner: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
