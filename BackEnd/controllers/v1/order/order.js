79991f99c0671ea93a83affd47d8e297f3ea98ab
kushagraupadhyay88 <73235511+kushagraupadhyay88@users.noreply.github.com>
2021-10-26 15:38:36 +0530

    kushagra

--- BackEnd/controllers/v1/order/order.js
+++ BackEnd/controllers/v1/order/order.js
@@ -1,15 +1,16 @@
 const Item = require("../../../models/Order.js");
+const jwt = require("jsonwebtoken");
 const addItem = async function (req, res) {
-  const amount = req.body.amount;
-  const acquired = req.body.acquired;
-  const owner = req.body.owner;
-  const item = new Item({
+    const amount = req.body.amount;
+    const acquired = req.body.acquired;
+    const owner = req.body.owner;
+    const item = new Item({
     amount: amount,
     acquired: acquired,
     owner: owner,
   });
-  await item.save();
-  res.status(200).send({
+    await item.save();
+    res.status(200).send({
     message: "Created Successfully",
     itemData: {
       item,
@@ -18,12 +19,13 @@ const addItem = async function (req, res) {
 };
 
 const showOrders = async function (_, res) {
-  Item.find()
-    .then((result) => {
+  await Item.findOne({
+    acquired: false,
+  }).then((result) => {
       res.status(200).json({
         order: result,
       });
-    })
+    }) 
     .catch((err) => {
       console.log(err);
       res.status(500).json({ error: err });
