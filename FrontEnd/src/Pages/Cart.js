851adb49e2d9fc305f575bc51e9ffde6bf7b7419
Milind Phalke <milindphalke30@gmail.com>
2021-10-31 12:26:11 +0530

    Deployed build version

--- FrontEnd/src/Pages/Cart.js
+++ FrontEnd/src/Pages/Cart.js
@@ -113,7 +113,7 @@ class CartPage extends Component {
       return;
     }
     Axios.post(
-      "http://localhost:5000/posts/addOrder",
+      "https://tapwaste.herokuapp.com/posts/addOrder",
       {
         amount: this.state.estimatedPrice,
         acquired: false,
